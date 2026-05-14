617d2d208b64b5285aba6d0e0a4ca1a3cac95144
Olivia <Olivialiu612@gmail.com>
2026-05-14 16:42:28 -0400

    initial commit

---
+++ test1.py
@@ -0,0 +1,9 @@
+import sys
+print(sys.executable)
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+print("pandas", pd.__version__)
+print("numpy", np.__version__)
+print("matplotlib", plt.matplotlib.__version__)
